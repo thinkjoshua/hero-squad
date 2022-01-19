adbab469904b87589b66db90283a425bc5c28c27
jmusau62-gmail <jmusau62@gmail.com>
2022-01-19 20:23:03 +0300

    added sufficient tests

--- src/test/java/models/HeroTest.groovy
+++ src/test/java/models/HeroTest.groovy
@@ -6,78 +6,54 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-@SuppressWarnings("ALL")
+
 public class HeroTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    private Hero setupNewHero(){
-        return new Hero ("Batman",45,"Money","Loneliness");
-    }
-
-    private Hero setupHero2() {
-        return new Hero("Superman", 180, "Almost everything", "Kryptonite");
-    }
-
     @Test
-    public void heroInstantiatesCorrectly() {
-        Hero newHero = setupNewHero();
+    public void newHero_instantiatesCorrectly_true() {
+        Hero newHero = Hero.setUpNewHero();
         assertTrue(newHero instanceof Hero);
     }
-
     @Test
-    public void getHeroName_true() {
-        Hero newHero = setupNewHero();
-        assertTrue(newHero.getName() instanceof String);
+    public void newHero_getName_String() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals("Arnold",newHero.getName());
     }
-
     @Test
-    public void getHeroAge_int() {
-        Hero newHero = setupNewHero();
-        assertEquals(45, newHero.getAge());
+    public void newHero_getAge_Int() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals(23,newHero.getAge());
     }
-
     @Test
-    public void getHeroPower_true() {
-        Hero newHero = setupNewHero();
-        assertTrue(newHero.getPower() instanceof String);
+    public void newHero_getPower_String() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals("flying",newHero.getPower());
     }
-
     @Test
-    public void getHeroWeakness_true() {
-        Hero newHero = setupNewHero();
-        assertTrue(newHero.getWeakness() instanceof String);
+    public void newHero_getWeakness_String() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals("fire",newHero.getWeakness());
     }
-
     @Test
-    public void getHeroList_storeTwoHeroes_true() {
-        Hero newHero = setupNewHero();
-        Hero anotherNewHero = setupHero2();
-        assertTrue(Hero.getHeroRegistry().contains(newHero));
-        assertTrue(Hero.getHeroRegistry().contains(anotherNewHero));
+    public void newHero_getAllInstances_true() {
+        Hero newHero = Hero.setUpNewHero();
+        Hero another = Hero.setUpNewHero();
+        assertTrue(Hero.getAllInstances().contains(newHero));
+        assertTrue(Hero.getAllInstances().contains(another));
     }
-
     @Test
-    public void findHero_searchForHeroById_String() {
-        Hero newHero = setupNewHero();
-        Hero anotherNewHero = setupHero2();
-        assertEquals("Batman", Hero.findHero(newHero.getHeroID()).getName());
+    public void newHero_getId_Int() {
+        Hero.clearAllHeroes();
+        Hero newHero = Hero.setUpNewHero();
+        Hero another = Hero.setUpNewHero();
+        Hero another1 = Hero.setUpNewHero();
+        assertEquals(3,another1.getId());
     }
-
     @Test
-    public void deleteSingleHero() {
-        Hero newHero = setupNewHero();
-        Hero anotherNewHero = setupHero2();
-        Hero thirdHero = new Hero("Iron Man", 35, "Money", "Ego");
-        Hero.deleteHero(anotherNewHero.getHeroID());
-        assertFalse(Hero.getHeroRegistry().contains(anotherNewHero));
-        assertEquals(3, Hero.getHeroRegistry().get(1).getHeroID());
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        Hero.clearHeroRegistry();
+    public void newHero_findById_id() {
+        Hero.clearAllHeroes();
+        Hero newHero = Hero.setUpNewHero();
+        Hero another = Hero.setUpNewHero();
+        assertEquals(2,Hero.findById(another.getId()).getId());
     }
 }
--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -1,113 +1,79 @@
-
 package models;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-@SuppressWarnings("ALL")
 public class SquadTest {
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    private Hero setupNewHero() {
-        return new Hero("Batman", 45, "Money", "Loneliness");
-    }
-
-    private Hero setupHero2() {
-        return new Hero("Iron Man", 30, "Money", "Ego");
-    }
-
-    private Squad setupNewSquad(Hero hero) {
-        return new Squad("Justice League", "Just Exist", hero);
-    }
-
-    private Squad setupNewSquad2(Hero hero) {
-        return new Squad("Avengers", "Just Exist again", hero);
+    @Test
+    public void newSquad_instantiatesCorrectly_true() {
+        Squad newSquad = Squad.setUpNewSquad();
+        assertTrue(newSquad instanceof Squad);
     }
 
     @Test
-    public void squadInstanciatedCorrectly() {
-        Squad squad = setupNewSquad(setupNewHero());
-        assertTrue(squad instanceof Squad);
+    public void newSquad_getName_String() {
+        Squad newSquad = Squad.setUpNewSquad();
+        assertEquals("Avengers", newSquad.getSquadName());
     }
 
     @Test
-    public void getSquadName() {
-        Squad squad = setupNewSquad(setupNewHero());
-        assertTrue(squad.getName() instanceof String);
+    public void newSquad_getSize_Int() {
+        Squad newSquad = Squad.setUpNewSquad();
+        assertEquals(5, newSquad.getSize());
     }
 
     @Test
-    public void getSquadCause() {
-        Squad squad = setupNewSquad(setupNewHero());
-        assertTrue(squad.getCause() instanceof String);
+    public void newSquad_getPower_String() {
+        Squad newSquad = Squad.setUpNewSquad();
+        assertEquals("Infinity Stone", newSquad.getCause());
     }
 
     @Test
-    public void addingHeroToExistingSquad() {
-        Squad squad = setupNewSquad(setupNewHero());
-        squad.addMembers(setupHero2());
-        assertEquals(2, squad.getMembers().size());
+    public void newSquad_getInstances_true() {
+        Squad newSquad = Squad.setUpNewSquad();
+        Squad another = Squad.setUpNewSquad();
+        assertTrue(Squad.getInstances().contains(newSquad));
+        assertTrue(Squad.getInstances().contains(another));
     }
 
     @Test
-    public void addHero_NotExeedLimitOf1_int() {
-        /* Change back limit to 1 before testing again -- Current Limit is 3 hence test below */
-        Squad squad = setupNewSquad(setupNewHero());
-        squad.addMembers(setupHero2());
-        assertEquals(2, squad.getMembers().size());
+    public void newSquad_getSquadMembers_Array() {
+        Squad newSquad = Squad.setUpNewSquad();
+        Hero newHero = Hero.setUpNewHero();
+        Hero newHero1 = Hero.setUpNewHero1();
+        newSquad.setSquadMembers(newHero);
+        assertEquals("Arnold", newSquad.getSquadMembers().get(0).getName());
     }
 
     @Test
-    public void addHero_NotExeedLimitOf3_int() {
-        Squad squad = setupNewSquad(setupNewHero());
-        squad.addMembers(setupHero2());
-        squad.addMembers(new Hero("1", 1, "1", "1"));
-        squad.addMembers(new Hero("2", 1, "1", "1"));
-        squad.addMembers(new Hero("3", 1, "1", "1"));
-        squad.addMembers(new Hero("4", 1, "1", "1"));
-        assertEquals(3, squad.getMembers().size());
-        assertTrue(squad.getSquadFull());
+    public void newSquad_allTestSquadMembers_Array() {
+        Hero newHero = Hero.setUpNewHero();
+        Squad newSquad = Squad.setUpNewSquad();
+        newSquad.clearAllSquadMembers();
+        newSquad.getSquadMembers().add(newHero);
+        newSquad.getSquadMembers().add(newHero);
+        assertEquals("Arnold", newSquad.getSquadMembers().get(0).getName());
     }
 
     @Test
-    public void removeHeroMember() {
-        Hero hero1 = setupNewHero();
-        Hero hero2 = setupHero2();
-        Squad squad = setupNewSquad(hero1);
-        squad.addMembers(hero2);
-        squad.removeMember(hero1);
-        assertFalse(squad.getMembers().contains(hero1));
+    public void addMember_addsMemberToSquad_Hero() {
+        Hero newHero = Hero.setUpNewHero();
+        Squad testSquad = Squad.setUpNewSquad();
+        Squad newSquad = Squad.findBySquadId(1);
+        newSquad.clearAllSquadMembers();
+        newSquad.getSquadMembers().add(newHero);
+        newSquad.getSquadMembers().add(newHero);
+        assertEquals(2, newSquad.getSquadMembers().size());
     }
 
     @Test
-    public void moveHeroToAnotherSquad() {
-        Hero.clearHeroRegistry();
-        Hero hero1 = setupNewHero();
-        Hero hero2 = setupHero2();
-        Squad squad = setupNewSquad(hero1);
-        Squad squad2 = setupNewSquad(hero2);
-        System.out.println("------------ Before -----------");
-        System.out.println(squad.getMembers());
-        System.out.println(squad2.getMembers());
+    public void setNewMember_hero() {
+        Hero.clearAllHeroes();
+        Hero newHwero = Hero.setUpNewHero();
+        Squad testSquad = Squad.setUpNewSquad();
+        testSquad.setSquadMembers(newHwero);
 
-        assertEquals(1, squad.getMembers().size());
-        squad.changeHeroSquad(hero1, squad2);
-
-        System.out.println("------------ After-----------");
-        System.out.println(squad.getMembers());
-        System.out.println(squad2.getMembers());
-        assertEquals(0, squad.getMembers().size());
-        assertEquals(2, squad2.getMembers().size());
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        Hero.clearHeroRegistry();
+        assertEquals(1, testSquad.getSquadMembers().get(0).getId());
     }
 }
